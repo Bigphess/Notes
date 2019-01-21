43274ddd75e5715e9b80612f9dec26f2c9a46cf7
Bigphess <xrp0308@gmail.com>
2019-01-21 10:37:41 +0900

    notes for opencv

---
+++ OpenCV/array.py
@@ -0,0 +1,5 @@
+import numpy as np 
+
+a = np.zeros((2,2), dtype = [('x', 'i4'), ('y', 'f4')])  
+
+print(a)
--- OpenCV/polor.py
+++ OpenCV/polor.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 
 
+video_path = '/Users/bigphess/Desktop/omnidirection/res/rabbit_250fps.mp4'
 cap = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(video_path)
 
 while True:
-	ret, frame = cap.read()
+	ret, frame = cap2.read()
 	# image = cv2.imread('/Users/bigphess/Downloads/IMG_6453.JPG')
 	
 
@@ -15,16 +17,20 @@ while True:
 		print("successful write")
 		break
 
-	h,w,ch = debug.shape
+	# h,w,ch = debug.shape
 
-	# print(type(debug.shape[0]))
-	# break
-	debug = cv2.logPolar(frame,(debug.shape[0]/2,debug.shape[1]/2),100,cv2.WARP_FILL_OUTLIERS)
+	print('the size of the frame is {} and {}'.format(debug.shape[1],debug.shape[0]))
+	cv2.imshow('??', frame)
+	cv2.waitKey(0)
+	break
+
+	
+	# debug = cv2.logPolar(frame,(debug.shape[0]/2,debug.shape[1]/2),100,cv2.WARP_FILL_OUTLIERS)
 
 
 	
-	cv2.imshow('??',debug)
 	
-	cv2.waitKey(1)
+	
+	# cv2.waitKey(1)
 
 
